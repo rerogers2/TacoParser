1fc59b1f1a8ce459feaf17b421594b2f1c6f7f6b
Robert Rogers <rerogers@gmail.com>
2023-04-04 19:12:41 -0500

    first update

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -23,21 +23,37 @@ namespace LoggingKata.Test
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        public void ShouldParseLongitude(string line, double expected)
+        public void ShouldParseLongitude(string line, double expected) // index 1
         {
             // TODO: Complete - "line" represents input data we will Parse to
             //       extract the Longitude.  Your .csv file will have many of these lines,
             //       each representing a TacoBell location
 
             //Arrange
+            var tacoParserLong = new TacoParser();
 
             //Act
+            var actual = tacoParserLong.Parse(line).Location.Longitude;
 
             //Assert
+            Assert.Equal(expected, actual);
         }
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude (string line, double expected) // index 0
+        {
+            //Arrange
+            var tacoParserLat = new TacoParser();
+
+            //Act
+            var actual = tacoParserLat.Parse(line).Location.Latitude;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
 
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,4 +1,6 @@
-﻿namespace LoggingKata
+﻿using System;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the Taco Bells
@@ -18,27 +20,40 @@
             if (cells.Length < 3)
             {
                 // Log that and return null
+                logger.LogWarning("Missing data: needs latitude, longitude, and a location.");
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
             // grab the latitude from your array at index 0
+            var latitude = Convert.ToDouble(cells[0]);
             // grab the longitude from your array at index 1
+            var longitude = Convert.ToDouble(cells[1]);
             // grab the name from your array at index 2
+            var name = cells[2];
 
             // Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
 
             // You'll need to create a TacoBell class
             // that conforms to ITrackable
+            var tacoBell = new TacoBell();
 
             // Then, you'll need an instance of the TacoBell class
             // With the name and point set correctly
+            // stored lat and long into one point and stored that point into the location
+            var point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
+
+            tacoBell.Name = name;
+            tacoBell.Location = point;
+
 
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
